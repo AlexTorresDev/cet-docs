1ca62905b2b0740cc0fa9895525c05438fc2a982
Alex Torres <alextorressk@gmail.com>
2023-05-01 22:45:02 -0500

    Change deploy url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), react(), tailwind()],
-  site: `https://alextorresdev.github.io/cet-docs`,
-  base: '/cet-docs/',
+  site: 'https://cet.alextrs.dev',
 });
